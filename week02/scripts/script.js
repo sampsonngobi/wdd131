f3711d156e33ee8d444854340470df0e10ca9056
dungasamsungo <sampsonngobi@gmail.com>
2024-09-26 15:08:41 -0300

    initial commit for week 2 BOM APP

---
+++ week02/scripts/script.js
@@ -0,0 +1,13 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("favchap");
+const list = document.querySelector("#list");
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+deleteButton.textContent = '❌'
+
+li.append(deleteButton);
+
+list.append(li);
