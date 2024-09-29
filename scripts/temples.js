6c41ed220464b5e5cf6adb1af464769ebfd5f35d
dungasamsungo <sampsonngobi@gmail.com>
2024-09-29 19:53:44 -0300

    larger screen ready with some setbacks

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,6 +4,8 @@ const hambutton = document.querySelector('#menu');
 hambutton.addEventListener('click', () =>{
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
+
+   
 });
 
 
