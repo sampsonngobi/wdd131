4fbf1b8d468d63ffdefe18af8abf4a32f8c9cb1c
dungasamsungo <sampsonngobi@gmail.com>
2024-09-29 12:13:41 -0300

    Phone size ready

---
+++ scripts/temples.js
@@ -0,0 +1,18 @@
+const mainnav = document.querySelector('nav');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () =>{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.querySelector('#currentyear').innerHTML = currentYear;
+
+// Get the last modified date of the document
+const lastModified = document.lastModified;
+
+// Select the element with id 'modified-date' and set its innerHTML
+document.querySelector('#modified-date').innerHTML = lastModified;
